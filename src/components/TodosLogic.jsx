7a5935c6337b12b476e496a0d8e33ce1d6bc6a31
sahlminkok <sahlminkok7@gmail.com>
2023-06-23 09:23:31 +0100

    Handle the input event

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,21 +1,23 @@
 import InputTodo from "./InputTodo";
 import TodoList from "./TodosList";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: "Setup development environment",
       completed: false,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: "Develop website and add content",
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: "Deploy to live server",
       completed: false,
     },
@@ -34,6 +36,15 @@ const TodosLogic = () => {
     );
   };
 
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  }
+
   const delTodo = (id) => {
     setTodos([
       ...todos.filter((todo) => {
@@ -43,7 +54,7 @@ const TodosLogic = () => {
   };
   return (
     <div>
-      <InputTodo />
+      <InputTodo addTodoItem={addTodoItem}/>
       <TodoList
         todosProps={todos}
         handleChange={handleChange}
