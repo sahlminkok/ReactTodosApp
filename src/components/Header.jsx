462a16705b9f62bedcc1ebf668fa02f2ddfce4d8
sahlminkok <sahlminkok7@gmail.com>
2023-06-25 21:54:03 +0100

    Add prop types to input todo component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import styles from "@/styles/Header.module.css";
 
 const Header = () => {
   return (
-    <header /*style={ headerStyle }*/ className={styles.header}>
+    <header className={styles.header}>
       <h1>todos</h1>
       <p>Items will persist in the browser local storage</p>
     </header>
